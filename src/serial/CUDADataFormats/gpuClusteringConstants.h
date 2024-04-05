586e3d6409a9edf026323ff0db3440ecb2b6c28d
Simone Balducci <simone.balducci00@gmail.com>
2024-04-05 14:37:01 +0200

    Adjust gpuCluster and CA constants

--- src/serial/CUDADataFormats/gpuClusteringConstants.h
+++ src/serial/CUDADataFormats/gpuClusteringConstants.h
@@ -5,10 +5,12 @@
 
 namespace pixelGPUConstants {
 #ifdef GPU_SMALL_EVENTS
-  constexpr uint32_t maxNumberOfHits = 24 * 1024;
+  /* constexpr uint32_t maxNumberOfHits = 24 * 1024; */
+  constexpr uint32_t maxNumberOfHits = 120000;		// max number of hits in a trackml event
 #else
-  constexpr uint32_t maxNumberOfHits =
-      48 * 1024;  // data at pileup 50 has 18300 +/- 3500 hits; 40000 is around 6 sigma away
+  /* constexpr uint32_t maxNumberOfHits = */
+  /*     48 * 1024;  // data at pileup 50 has 18300 +/- 3500 hits; 40000 is around 6 sigma away */
+  constexpr uint32_t maxNumberOfHits = 120000;		// max number of hits in a trackml event
 #endif
 }  // namespace pixelGPUConstants
 
